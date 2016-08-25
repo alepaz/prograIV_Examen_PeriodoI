28db9ddd6517df9a7666ffaf5081013f656042a8
Chrono <Chrono>
2016-08-24 23:09:58 -0600

    ._. an ortodox method but works

--- PrograIV_Examen_PT132129/Form1.cs
+++ PrograIV_Examen_PT132129/Form1.cs
@@ -64,11 +64,89 @@ namespace PrograIV_Examen_PT132129
             comboBox1.Items.Add("Estatura");
         }
 
+        public void BubbleSort(ref string[] s, ref int[] num) {
+            string temp = string.Empty;
+            int indiceTemp = 0;
+
+            for (int i = 1; i < s.Length; i++)
+                {
+                    for (int j = 0; j < s.Length - i; j++)
+                    {
+                        if (s[j].CompareTo(s[j + 1]) > 0)
+                        {
+                            indiceTemp = num[j];
+                            temp = s[j];
+
+                            num[j] = num[j + 1];
+                            s[j] = s[j + 1];
+
+                            num[j + 1] = indiceTemp;
+                            s[j + 1] = temp;
+                        }
+                    }
+                }
+            for (int i = 0; i < s.Length; i++)
+            {
+                MessageBox.Show(s[i] + " ");
+                MessageBox.Show(num[i] + " ");
+            }
+            }
+
+        public void BubbleSort(ref int[] s, ref int[] num)
+        {
+            int temp = 0;
+            int indiceTemp = 0;
+
+            for (int i = 1; i < s.Length; i++)
+            {
+                for (int j = 0; j < s.Length - i; j++)
+                {
+                    if (s[j].CompareTo(s[j + 1]) > 0)
+                    {
+                        indiceTemp = num[j];
+                        temp = s[j];
+
+                        num[j] = num[j + 1];
+                        s[j] = s[j + 1];
+
+                        num[j + 1] = indiceTemp;
+                        s[j + 1] = temp;
+                    }
+                }
+            }
+            for (int i = 0; i < s.Length; i++)
+            {
+                MessageBox.Show(s[i] + " ");
+                MessageBox.Show(num[i] + " ");
+            }
+        }
+
 
         private void button2_Click(object sender, EventArgs e)
         {
-            string metodo;
-            string forma;
+            //contador
+            int i = 0;
+            //arreglos, uno servira de espejo para realizar el merge en la 
+            int[] arreglo_numeros = new int[dataGridView1.Rows.Count] ;
+            //para almacenar los datos a ordenar
+            string[] column0Array = new string[dataGridView1.Rows.Count];
+            string[] column1Array = new string[dataGridView1.Rows.Count];
+            int[] column2Array = new int[dataGridView1.Rows.Count];
+            string[] column3Array = new string[dataGridView1.Rows.Count];
+
+            //Arrays con los datos para limpiar el datagridview
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                column0Array[i] = row.Cells[0].Value != null ? row.Cells[0].Value.ToString() : string.Empty;
+                MessageBox.Show(column0Array[i].ToString());
+                column1Array[i] = row.Cells[1].Value != null ? row.Cells[1].Value.ToString() : string.Empty;
+                column2Array[i] = row.Cells[2].Value != null ? int.Parse(row.Cells[2].Value.ToString()) : 0;
+                column3Array[i] = row.Cells[3].Value != null ? row.Cells[3].Value.ToString() : string.Empty;
+                arreglo_numeros[i] = i;
+                MessageBox.Show(arreglo_numeros[i].ToString());
+                i++;
+                
+            }
 
             //Para determinar el metodo de ordenamiento escogido por el usuario
             if (bsrbtn.Checked)
@@ -77,7 +155,17 @@ namespace PrograIV_Examen_PT132129
                 //Para determinar la forma de ordenar escodiga por el usuario
                 if (asrbtn.Checked)
                 {
-                    //BubbleSort & Ascendente
+
+                    BubbleSort(ref column0Array, ref arreglo_numeros);
+                    //BubbleSort(ref column0Array);
+
+                    dataGridView1.Rows.Clear();
+                    dataGridView1.Refresh();
+
+                    for (i = 0; i < arreglo_numeros.Length; i++) {
+                        dataGridView1.Rows.Add(column0Array[i], column1Array[arreglo_numeros[i]], column2Array[arreglo_numeros[i]], column3Array[arreglo_numeros[i]]);
+
+                    }
 
 
                 }
